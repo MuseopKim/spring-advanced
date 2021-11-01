94c651e6bd5e4fb20748a84a089ede02db021652
ragdoll <museopkim0214@gmail.com>
2021-11-01 23:54:23 +0900

    Custom logger

---
+++ src/main/java/com/springadvanced/app/v0/OrderRepositoryV0.java
@@ -0,0 +1,27 @@
+package com.springadvanced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import static java.lang.Thread.sleep;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+    public void save(String itemId) {
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("Exception!");
+        }
+
+        sleep(1000);
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/springadvanced/trace/hellotrace/HelloTraceV1.java
@@ -0,0 +1,55 @@
+package com.springadvanced.trace.hellotrace;
+
+import com.springadvanced.trace.TraceId;
+import com.springadvanced.trace.TraceStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class HelloTraceV1 {
+
+    private static final String START_PREFIX = "-->";
+    private static final String COMPLETE_PREFIX = "<--";
+    private static final String EX_PREFIX = "<x-";
+
+    public TraceStatus begin(String message) {
+        TraceId traceId = new TraceId();
+        long startTimeMs = System.currentTimeMillis();
+        log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX, traceId.getLevel()), message);
+        return new TraceStatus(traceId, startTimeMs, message);
+    }
+
+    public void end(TraceStatus status) {
+        complete(status, null);
+    }
+
+    public void exception(TraceStatus status, Exception exception) {
+        complete(status, exception);
+    }
+
+
+    private void complete(TraceStatus status, Exception exception) {
+        Long stopTimeMs = System.currentTimeMillis();
+        long resultTimeMs = stopTimeMs - status.getStartTimeMs();
+        TraceId traceId = status.getTraceId();
+
+        if (exception == null) {
+            log.info("[{}] {}{} time={}ms", traceId.getId(), addSpace(COMPLETE_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs);
+            return;
+        }
+
+        log.info("[{}] {}{} time={}ms ex={}", traceId.getId(), addSpace(EX_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs, exception.getMessage());
+    }
+
+    public static String addSpace(String prefix, int level) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < level; i++) {
+            stringBuilder.append((i == level - 1) ? "|" + prefix : "|   ");
+        }
+
+        return stringBuilder.toString();
+    }
+}
