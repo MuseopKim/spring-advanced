d1010e821d57ce91f1e7b21f9fb971cbf1bee955
ragdoll <museopkim0214@gmail.com>
2021-11-02 22:34:10 +0900

    Custom Logger 파라미터 동기화

---
+++ src/main/java/com/springadvanced/app/v2/v1/OrderControllerV2.java
@@ -0,0 +1,30 @@
+package com.springadvanced.app.v2.v1;
+
+import com.springadvanced.trace.TraceStatus;
+import com.springadvanced.trace.hellotrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+    private final HelloTraceV1 trace;
+
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+        TraceStatus status = null;
+
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(status.getTraceId(), itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception exception) {
+            trace.exception(status, exception);
+            throw exception;
+        }
+    }
+}
---
+++ src/main/java/com/springadvanced/app/v2/v1/OrderServiceV2.java
@@ -0,0 +1,28 @@
+package com.springadvanced.app.v2.v1;
+
+import com.springadvanced.trace.TraceId;
+import com.springadvanced.trace.TraceStatus;
+import com.springadvanced.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV2 {
+
+    private final OrderRepositoryV2 orderRepository;
+    private final HelloTraceV2 trace;
+
+    public void orderItem(TraceId traceId, String itemId) {
+        TraceStatus status = null;
+
+        try {
+            status = trace.beginSync(traceId, "OrderService.orderItem()");
+            orderRepository.save(status.getTraceId(), itemId);
+            trace.end(status);
+        } catch (Exception exception) {
+            trace.exception(status, exception);
+            throw exception;
+        }
+    }
+}
