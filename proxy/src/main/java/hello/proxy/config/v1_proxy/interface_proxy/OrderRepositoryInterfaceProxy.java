9cbb7579726550ce7055723b49044509db4cd331
ragdoll <museopkim0214@gmail.com>
2021-11-19 22:33:25 +0900

    Proxy 3

--- proxy/src/main/java/hello/proxy/config/v1_proxy/interface_proxy/OrderRepositoryInterfaceProxy.java
+++ proxy/src/main/java/hello/proxy/config/v1_proxy/interface_proxy/OrderRepositoryInterfaceProxy.java
@@ -1,22 +1,24 @@
-package hello.proxy.app.config.v1_proxy.interface_proxy;
+package hello.proxy.config.v1_proxy.interface_proxy;
 
-import hello.proxy.app.v1.OrderServiceV1;
+import hello.proxy.app.v1.OrderRepositoryV1;
 import hello.proxy.trace.TraceStatus;
 import hello.proxy.trace.logtrace.LogTrace;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class OrderServiceInterfaceProxy implements OrderServiceV1 {
+public class OrderRepositoryInterfaceProxy implements OrderRepositoryV1 {
 
-    private final OrderServiceV1 target;
+    private final OrderRepositoryV1 target;
     private final LogTrace logTrace;
 
     @Override
-    public void orderItem(String itemId) {
+    public void save(String itemId) {
         TraceStatus status = null;
+
         try {
-            status = logTrace.begin("OrderService.orderItem()");
-            target.orderItem(itemId);
+            status = logTrace.begin("OrderRepository.request()");
+
+            target.save(itemId);
             logTrace.end(status);
         } catch (Exception exception) {
             logTrace.exception(status, exception);
