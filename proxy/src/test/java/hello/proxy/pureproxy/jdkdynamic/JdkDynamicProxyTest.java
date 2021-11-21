a7ba0b27c6e2ea090dd7ef77beb831358f8b2409
ragdoll <museopkim0214@gmail.com>
2021-11-21 20:33:49 +0900

    JDK Dynamic Proxy 1

---
+++ proxy/src/test/java/hello/proxy/pureproxy/jdkdynamic/JdkDynamicProxyTest.java
@@ -0,0 +1,35 @@
+package hello.proxy.pureproxy.jdkdynamic;
+
+import hello.proxy.pureproxy.jdkdynamic.code.*;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Proxy;
+
+@Slf4j
+public class JdkDynamicProxyTest {
+
+    @Test
+    void dynamicA() {
+        AInterface target = new AImpl();
+        // 동적 프록시에 적용할 핸들러 로직
+        TimeInvocationHandler handler = new TimeInvocationHandler(target);
+
+        AInterface proxy = (AInterface) Proxy.newProxyInstance(AInterface.class.getClassLoader(), new Class[]{AInterface.class}, handler);
+        proxy.call();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+    }
+
+    @Test
+    void dynamicB() {
+        BInterface target = new BImpl();
+        // 동적 프록시에 적용할 핸들러 로직
+        TimeInvocationHandler handler = new TimeInvocationHandler(target);
+
+        BInterface proxy = (BInterface) Proxy.newProxyInstance(BInterface.class.getClassLoader(), new Class[]{BInterface.class}, handler);
+        proxy.call();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+    }
+}
