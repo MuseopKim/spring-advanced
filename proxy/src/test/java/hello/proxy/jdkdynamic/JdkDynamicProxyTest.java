32e523c98d7e4e60474fdc07b4bcf2ae8e528f18
ragdoll <museopkim0214@gmail.com>
2021-11-23 22:20:38 +0900

    CG Lib

--- proxy/src/test/java/hello/proxy/jdkdynamic/JdkDynamicProxyTest.java
+++ proxy/src/test/java/hello/proxy/jdkdynamic/JdkDynamicProxyTest.java
@@ -1,6 +1,6 @@
-package hello.proxy.pureproxy.jdkdynamic;
+package hello.proxy.jdkdynamic;
 
-import hello.proxy.pureproxy.jdkdynamic.code.*;
+import hello.proxy.jdkdynamic.code.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
