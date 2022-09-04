a317d7627bf8c6b02e429b936d12c2d4830a3927
ragdoll <museopkim0214@gmail.com>
2022-09-05 00:45:02 +0900

    `@Aspect` AOP

---
+++ proxy/src/main/java/hello/proxy/config/v6_aop/aspect/LogTraceAspect.java
@@ -0,0 +1,35 @@
+package hello.proxy.config.v6_aop.aspect;
+
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import java.lang.reflect.Method;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Slf4j
+@RequiredArgsConstructor
+@Aspect
+public class LogTraceAspect {
+
+    private final LogTrace logTrace;
+
+    @Around("execution(* hello.proxy.app..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        TraceStatus status = null;
+        try {
+            String message = joinPoint.getSignature().toShortString();
+            status = logTrace.begin(message);
+
+            Object result = joinPoint.proceed();
+
+            logTrace.end(status);
+            return result;
+        } catch (Exception exception) {
+            logTrace.exception(status, exception);
+            throw exception;
+        }
+    }
+}
